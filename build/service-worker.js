66299af65882953d2b76688813c4ecdaa890dd69
timelessover <shanpaodaxia@qq.com>
2019-05-04 20:55:10 +0800

    ?

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9954446c1b60e9f0f0f87aa97861b3e8.js"
+  "./precache-manifest.d28a41afa20f71c3a017086193bc6feb.js"
 );
 
 workbox.clientsClaim();
